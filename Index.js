c2ce1930834f57dfa456385a80d89fbc0ba79a7b
Codewithshimite <148382968+Codewithshimite@users.noreply.github.com>
2024-03-17 16:33:33 +0100

    Update Index.js

--- Index.js
+++ Index.js
@@ -10,10 +10,12 @@ const resetInput = document.querySelector('#reset')
 
 
 
+
 let height = heightInput.value;
 let weight = weightInput.value
 let sex = sexInput.value;
 let age = ageInput.value;
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -28,153 +30,88 @@ form.addEventListener("submit", (e) => {
     
 
     //    // for male of age 18-24
-if (index < 20 && age >= 18 && age <= 24  && sex === 'M' ){
+if (index < 20 && age >= 18 && age <= 24  && sex === 'Male' ){ 
     result.textContent = `${index}: you are underweight`
 
-}else if (index > 20 && index <= 25 && age >= 18 && age <= 24 && sex === 'M' ){
+}else if (index > 20 && index <= 25 && age >= 18 && age <= 24 && sex === 'Male' ){
     result.textContent = `${index}: you are Normal weight`
 
-}else if (index > 25 && index <= 30 && age >= 18 && age <= 24 && sex === 'M' ){
+}else if (index > 25 && index <= 30 && age >= 18 && age <= 24 && sex === 'Male' ){
     result.textContent = `${index}: you are Overweight`
 
-}else if (index > 30 && index <= 40 && age >= 18 && age <= 24 && sex ==='M' ){
+}else if (index > 30 && index <= 40 && age >= 18 && age <= 24 && sex ==='Male' ){
     result.textContent = `${index}: Adiposity`
 
-}else if (index > 40 && age >= 18 && age <= 24 && sex === 'M' ){
+}else if (index > 40 && age >= 18 && age <= 24 && sex === 'Male' ){
     result.textContent = `${index}: Severe adiposity`
 
 
-  // for male of age 18-24
-}if (index < 20 && age >= 18 && age <= 24  && sex === 'M' ){
-    result.textContent = `${index}: you are underweight`
 
-}else if (index > 20 && index <= 25 && age >= 18 && age <= 24 && sex === 'M' ){
-    result.textContent = `${index}: you are Normal weight`
-
-}else if (index > 25 && index <= 30 && age >= 18 && age <= 24 && sex === 'M' ){
-    result.textContent = `${index}: you are Overweight`
-
-}else if (index > 30 && index <= 40 && age >= 18 && age <= 24 && sex ==='M' ){
-    result.textContent = `${index}: Adiposity`
-
-}else if (index > 40 && age >= 18 && age <= 24 && sex === 'M' ){
-    result.textContent = `${index}: Severe adiposity`
-
-
-    
-    // for male of age 18-24
-}if (index < 20 && age >= 18 && age <= 24  && sex === 'M' ){
-   result.textContent =`${index}: you are underweight`
-
-}else if (index > 20 && index <= 25 && age >= 18 && age <= 24 && sex === 'M' ){
-    result.textContent =`${index}: you are Normal weight`
- }else if (index > 25 && index <= 30 && age >= 18 && age <= 24 && sex === 'M' ){
-    result.textContent =`${index}: you are Overweight`
-
- }else if (index > 30 && index <= 40 && age >= 18 && age <= 24 && sex ==='M' ){
-    result.textContent =`${index}: Adiposity`
-
- }else if (index > 40 && age >= 18 && age <= 24 && sex === 'M' ){
-     result.textContent =`${index}: Severe adiposity`
- 
-
-
-        // for male of age 18-24
- }if (index < 20 && age >= 18 && age <= 24  && sex === 'M' ){
-    result.textContent =`${index}: you are underweight`
-
-}else if (index > 20 && index <= 25 && age >= 18 && age <= 24 && sex === 'M' ){
-    result.textContent =`${index}: you are Normal weight`
-
-}else if (index > 25 && index <= 30 && age >= 18 && age <= 24 && sex === 'M' ){
-    result.textContent =`${index}: you are Overweight`
-
-}else if (index > 30 && index <= 40 && age >= 18 && age <= 24 && sex ==='M' ){
-    result.textContent =`${index}: Adiposity`
-
-}else if (index > 40 && age >= 18 && age <= 24 && sex === 'M' ){
-    result.textContent =`${index}: Severe adiposity`
-
-    
-}    // for male of age 18-24
-    if (index < 20 && age >= 18 && age <= 24  && sex === 'M' ){
-        result.textContent =`${index}: you are underweight`
-
-    }else if (index > 20 && index <= 25 && age >= 18 && age <= 24 && sex === 'M' ){
-        result.textContent =`${index}: you are Normal weight`
-
-    }else if (index > 25 && index <= 30 && age >= 18 && age <= 24 && sex === 'M' ){
-        result.textContent =`${index}: you are Overweight`
-
-    }else if (index > 30 && index <= 40 && age >= 18 && age <= 24 && sex ==='M' ){
-        result.textContent =`${index}: Adiposity`
-
-    }else if (index > 40 && age >= 18 && age <= 24 && sex === 'M' ){
-        result.textContent `${index}: Severe adiposity`
 
                 // for male of age 25-34
-    }else if(index < 21 && age >= 25 && age <= 34 && sex === 'M'){
+    }else if(index < 21 && age >= 25 && age <= 34 && sex === 'Male'){
         result.textContent =`${index}: you are underweight`
 
-    }else if (index > 21 && index <= 26 && age >= 25 && age <= 34 && sex === 'M'){
+    }else if (index > 21 && index <= 26 && age >= 25 && age <= 34 && sex === 'Male'){
         result.textContent =`${index}: you are Normal weight`
 
-    }else if (index > 26 && index <= 31 && age >= 25 && age <= 34 && sex === 'M'){
+    }else if (index > 26 && index <= 31 && age >= 25 && age <= 34 && sex === 'Male'){
         result.textContent =`${index}: you are overweight`
 
-    }else if (index > 31 && index <= 41 && age >= 25 && age <= 34 && sex === 'M'){
+    }else if (index > 31 && index <= 41 && age >= 25 && age <= 34 && sex === 'Male'){
         result.textContent =`${index}: Adiposity`
 
-    }else if  (index > 41 && age >= 25 && age <= 34 && sex === 'M'){
+    }else if  (index > 41 && age >= 25 && age <= 34 && sex === 'Male'){
         result.textContent =`${index}:  Severe adiposity`
 
  
         // for male of age 35-44
-    }else if (index < 22 && age >= 35 && age <= 44 && sex === 'M'){
+    }else if (index < 22 && age >= 35 && age <= 44 && sex === 'Male'){
         result.textContent =`${index}: you are Overweight`
 
-    }else if (index > 22 && index <= 27 && age >= 35 && age <= 44 && sex === 'M'){
+    }else if (index > 22 && index <= 27 && age >= 35 && age <= 44 && sex === 'Male'){
         result.textContent =`${index}: you are Normal weight`
 
-    }else if (index > 27 && index <= 32 && age >= 35 && age <= 44 && sex === 'M'){
+    }else if (index > 27 && index <= 32 && age >= 35 && age <= 44 && sex === 'Male'){
         result.textContent =`${index}: you are overweight`
 
-    }else if (index > 32 && index <= 42 && age >= 35 && age <= 44 && sex === 'M'){
+    }else if (index > 32 && index <= 42 && age >= 35 && age <= 44 && sex === 'Male'){
         result.textContent =`${index}: Adiposity`
 
-    }else if (index > 44 && age >= 35 && age <= 44 && sex === 'M'){
+    }else if (index > 44 && age >= 35 && age <= 44 && sex === 'Male'){
         result.textContent =`${index}:  Severe adiposity`
 
 
 
 
          // for male of age 45-54
-     }else if (index < 23 && age >= 45 && age <= 54 && sex === 'M'){
+     }else if (index < 23 && age >= 45 && age <= 54 && sex === 'Male'){
         result.textContent =`${index}}: you are Underweight`
 
-    }else if (index > 23 && index <= 28 && age >= 45 && age <= 54 && sex === 'M'){
+    }else if (index > 23 && index <= 28 && age >= 45 && age <= 54 && sex === 'Male'){
         result.textContent =`${index}: you are Normal weight`
-     }else if (index > 28 && index <= 33 && age >= 45 && age <= 54 && sex === 'M'){
+     }else if (index > 28 && index <= 33 && age >= 45 && age <= 54 && sex === 'Male'){
      result.textContent =`${index}: you are Overweight`
 
-     }else if  (index > 33 && index <= 43 && age >= 45 && age <= 54 && sex === 'M'){
+     }else if  (index > 33 && index <= 43 && age >= 45 && age <= 54 && sex === 'Male'){
          result.textContent =`${index}: Adiposity`
 
-    }else if (index > 43 && age >= 45 && age <= 54 && sex === 'M' ){
+    }else if (index > 43 && age >= 45 && age <= 54 && sex === 'Male' ){
         result.textContent =`${index}:severe adiposity`
 
 
+        
           // for male of age 55-64
-    } else if (index < 24 && age >= 55 && age <= 64 && sex === 'M' ){
+    } else if (index < 24 && age >= 55 && age <= 64 && sex === 'Male' ){
     result.textContent =`${index}: you are Underweight`
 
-    }else if (index > 24 && index <= 29 && age >= 55 && age <= 64 && sex === 'M' ){
+    }else if (index > 24 && index <= 29 && age >= 55 && age <= 64 && sex === 'Male' ){
     result.textContent =`${index}: you are Normal weight`
 
-    }else if (index > 29 && index <= 34 && age >= 55 && age <= 64 && sex === 'M' ){
+    }else if (index > 29 && index <= 34 && age >= 55 && age <= 64 && sex === 'Male' ){
     result.textContent =`${index}: You are Overweight`
 
-    }else if (index > 34 && index <= 44 & age >= 55 && age <= 64 && sex === 'M'){
+    }else if (index > 34 && index <= 44 & age >= 55 && age <= 64 && sex === 'Male'){
     result.textContent =`${index}: Adiposity`
 
     }else if (index > 44 && age >= 55 && age <= 64 && sex === "M" ){
@@ -184,20 +121,23 @@ if (index < 20 && age >= 18 && age <= 24  && sex === 'M' ){
 
          // for male of age above 65+
 
-    }else if (index < 25 && age > 65 && sex === 'M'){
+    }else if (index < 25 && age > 65 && sex === 'Male'){
         result.textContent =`${index}: you are Underweight`
 
-    }else if (index > 25 && index <= 30 && age > 65 && sex =='M'){
+    }else if (index > 25 && index <= 30 && age > 65 && sex =='Male'){
         result.textContent =`${index}: you are Normal weight`
 
-    }else if (index > 30 && index <= 35 && age > 65 && sex === 'M'){
+    }else if (index > 30 && index <= 35 && age > 65 && sex === 'Male'){
         result.textContent =`${index}: You are Overweight`
 
-    }else if (index > 35 && index <= 45 && age > 65 && sex === 'M'){
+    }else if (index > 35 && index <= 45 && age > 65 && sex === 'Male'){
         result.textContent =`${index}: Adiposity`
 
-    }else if (index > 45 && age > 65 && sex === 'M' ){
+    }else if (index > 45 && age > 65 && sex === 'Male' ){
         result.textContent =`${index}:Severe adiposity`
+
+
+
 
         
 
@@ -205,111 +145,111 @@ if (index < 20 && age >= 18 && age <= 24  && sex === 'M' ){
                  //WOMEN
 
         //For Female age 18-24
-        }else if (index < 19 && age >= 18 && age <= 24 & sex === 'F'){
+        }else if (index < 19 && age >= 18 && age <= 24 & sex === 'Female' ){
         result.textContent =`${index}: you are Underweight`
 
-         }else if (index > 19 && index <= 24 && age >= 18 && age <= 24 && sex === 'F'){
+         }else if (index > 19 && index <= 24 && age >= 18 && age <= 24 && sex === 'Female'){
         result.textContent =`${index}: you are Normal weight`
 
-        }else if (index > 24 && index <= 29 && age >= 18 && age <= 24 && sex === 'F'){
+        }else if (index > 24 && index <= 29 && age >= 18 && age <= 24 && sex === 'Female'){
         result.textContent =`${index}: You are Overweight`
 
-        }else if (index > 29 && index <= 39 && age >= 18  && age <= 24 && sex === 'F'){
+        }else if (index > 29 && index <= 39 && age >= 18  && age <= 24 && sex === 'Female'){
         result.textContent =`${index}: Adiposity`
 
-        }else if (index > 39 && age >= 18 && age <= 24 && sex === 'F'){
+        }else if (index > 39 && age >= 18 && age <= 24 && sex === 'Female'){
         result.textContent =`${index}:Severe adiposity`
 
 
           //For Female age 25-34
 
-        }else if (index < 20 && age >= 25 && age <= 34 & sex === 'F'){
+        }else if (index < 20 && age >= 25 && age <= 34 & sex === 'Female'){
             result.textContent =`${index}: you are Underweight`
     
-        }else if (index > 20 && index <= 25 && age >= 25 && age <= 34 && sex === 'F'){
+        }else if (index > 20 && index <= 25 && age >= 25 && age <= 34 && sex === 'Female'){
             result.textContent =`${index}: you are Normal weight`
     
-        }else if (index > 25 && index <= 30 && age >= 25 && age <= 34 && sex === 'F'){
+        }else if (index > 25 && index <= 30 && age >= 25 && age <= 34 && sex === 'Female'){
             result.textContent =`${index}: You are Overweight`
     
-        }else if (index > 30 && index <= 40 && age >= 25  && age <= 34 && sex === 'F'){
+        }else if (index > 30 && index <= 40 && age >= 25  && age <= 34 && sex === 'Female'){
             result.textContent =`${index}: Adiposity`
     
-        }else if (index > 40 && age >= 25 && age <= 34 && sex === 'F'){
+        }else if (index > 40 && age >= 25 && age <= 34 && sex === 'Female'){
             result.textContent =`${index}:Severe adiposity`
 
 
 
                     // //For Female age 35-44
-    }else if (index < 21 && age >= 35 && age <= 44 & sex === 'F'){
+    }else if (index < 21 && age >= 35 && age <= 44 & sex === 'Female'){
         result.textContent =`${index}: you are Underweight`
 
-    }else if (index > 21 && index <= 26 && age >= 35 && age <= 44 && sex === 'F'){
+    }else if (index > 21 && index <= 26 && age >= 35 && age <= 44 && sex === 'Female'){
         result.textContent =`${index}: you are Normal weight`
 
-    }else if (index > 26 && index <= 31 && age >= 35 && age <= 44 && sex === 'F'){
+    }else if (index > 26 && index <= 31 && age >= 35 && age <= 44 && sex === 'Female'){
         result.textContent =`${index}: You are Overweight`
 
-    }else if (index > 31 && index <= 41 && age >= 35  && age <= 44 && sex === 'F'){
+    }else if (index > 31 && index <= 41 && age >= 35  && age <= 44 && sex === 'Female'){
         result.textContent =`${index}: Adiposity`
 
-    }else if (index > 40 && age >= 35 && age <= 44 && sex === 'F'){
+    }else if (index > 40 && age >= 35 && age <= 44 && sex === 'Female'){
         result.textContent =`${index}:Severe adiposity`
 
 
         
          // //For Female age 45-54
 
-    }else if (index < 22 && age >= 45 && age <= 54 & sex === 'F'){
+    }else if (index < 22 && age >= 45 && age <= 54 & sex === 'Female'){
         result.textContent =`${index}: you are Underweight`
 
-    }else if (index > 22 && index <= 27 && age >= 45 && age <= 54 && sex === 'F'){
+    }else if (index > 22 && index <= 27 && age >= 45 && age <= 54 && sex === 'Female'){
         result.textContent =`${index}: you are Normal weight`
 
-    }else if (index > 27 && index <= 32 && age >= 45 && age <= 54 && sex === 'F'){
+    }else if (index > 27 && index <= 32 && age >= 45 && age <= 54 && sex === 'Female'){
         result.textContent =`${index}: You are Overweight`
 
-    }else if (index > 42 <= 42 && age >= 45  && age <= 54 && sex === 'F'){
+    }else if (index > 42 <= 42 && age >= 45  && age <= 54 && sex === 'Female'){
         result.textContent =`${index}: Adiposity`
 
-    }else if (index > 42 && age >= 45 && age <= 54 && sex === 'F'){
+    }else if (index > 42 && age >= 45 && age <= 54 && sex === 'Female'){
         result.textContent =`${index}:Severe adiposity`
 
 
                 // //For Female age 55-64
 
-    }else if (index < 23 && age >= 55 && age <= 64 & sex === 'F'){
+    }else if (index < 23 && age >= 55 && age <= 64 & sex === 'Female'){
         result.textContent =`${index}: you are Underweight`
 
-    }else if (index > 23 && index <= 28 && age >= 55 && age <= 64 && sex === 'F'){
+    }else if (index > 23 && index <= 28 && age >= 55 && age <= 64 && sex === 'Female'){
         result.textContent =`${index}: you are Normal weight`
 
-    }else if (index > 28 && index <= 33 && age >= 55 && age <= 64 && sex === 'F'){
+    }else if (index > 28 && index <= 33 && age >= 55 && age <= 64 && sex === 'Female'){
         result.textContent =`${index}: You are Overweight`
 
-    }else if (index > 33 <= 43 && age >= 55  && age <= 64 && sex === 'F'){
+    }else if (index > 33 <= 43 && age >= 55  && age <= 64 && sex === 'Female'){
         result.textContent =`${index}: Adiposity`
 
-    }else if (index > 43 && age >= 55 && age <= 64 && sex === 'F'){
+    }else if (index > 43 && age >= 55 && age <= 64 && sex === 'Female'){
         result.textContent =`${index}:Severe adiposity`
 
 
         
          // //For Female age 65+
 
-    }else if (index < 24 && age >= 65 & sex === 'F'){
+    }else if (index < 24 && age >= 65 & sex === 'Female'){
         result.textContent =`${index}: you are Underweight`
 
-    }else if (index > 24 && index <= 29 && age >= 65 && sex === 'F'){
+    }else if (index > 24 && index <= 29 && age >= 65 && sex === 'Female'){
         result.textContent =`${index}: you are Normal weight`
 
-    }else if (index > 29 && index <= 34 && age >= 65 && sex === 'F'){
+    }else if (index > 29 && index <= 34 && age >= 65 && sex === 'Female'){
         result.textContent =`${index}: You are Overweight`
 
-    }else if (index > 34 <= 44 && age >= 65 && sex === 'F'){
+    }else if (index > 34 <= 44 && age >= 65 && sex === 'Female'){
         result.textContent =`${index}: Adiposity`
 
-    }else if (index > 44 && age >= 65  && sex === 'F'){
+    }else if (index > 44 && age >= 65  && sex === 'Female'){
         result.textContent =`${index}:Severe adiposity`
 
     }else {
